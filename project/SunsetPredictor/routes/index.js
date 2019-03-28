b80fd8b4ba613e839f01e179dba1689121d87031
Dennis Henneman <dhennem@bu.edu>
2019-03-27 22:37:25 -0400

    set up to make API call

--- project/SunsetPredictor/routes/index.js
+++ project/SunsetPredictor/routes/index.js
@@ -1,33 +1,27 @@
+
 var express = require('express');
 var router = express.Router();
-//var request = require(request);
 var SunsetWx = require('node-sunsetwx');
+const request = require("request");
+
+
+const callback = (err, res) => console.log("Error: " + err + "Result " + res);
 
 /* GET home page. (called by default) */
 router.get('/', function(req, res, next) {
+  sunburstQuery("-40", "40");
   res.render('index', {});
-
-  sunburstLogin();
-
 });
 
-function sunburstLogin() {
-
-  var sunsetwx = new SunsetWx({
-    email: 'xxx',
-    password: "xxxxx"
-  });
-
-  console.log(sunsetwx);
-
+function sunburstQuery(lat, long) {
 
   /*
   try {
     const sunburst = new SunburstJS();
 
     const session = await sunburst.createSession({
-      email: 'dhennem@bu.edu',
-      password: 'cs411isawesome',
+      email: 'email@example.com',
+      password: '3sLWT-Xt*$FD_SMX4F',
       type: 'permanent',
       scope: ['predictions']
     });
@@ -37,10 +31,25 @@ function sunburstLogin() {
   } catch (ex) {
     // Handle general network or parsing errors.
     return console.error(ex);
-  }
+  };
   */
 
+  var sunsetwx = new SunsetWx({
+    email: 'dhennem@bu.edu',
+    password: 'cs411isawesome'
+  });
+
+  console.log(sunsetwx.valueOf());
+
+  /*
+  sunsetwx.quality({
+    coords: '-77.331536,43.271152',
+    type: 'sunset'
+  }, callback)
+*/
+
 };
+
 
 /* post prediction result page (called by submitting the form)*/
 router.post('/predict', function (req, res, next) {
