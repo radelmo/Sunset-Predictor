c2bee5b6f36fb9f9ac63608a54ac5a422c9d0875
Dennis Henneman <dhennem@bu.edu>
2019-04-25 16:39:02 -0400

    front end finished for making and viewing posts

--- project/SunsetPredictor/routes/index.js
+++ project/SunsetPredictor/routes/index.js
@@ -17,6 +17,16 @@ router.get('/', function(req, res, next) {
   res.render('index', {});
 });
 
+/*Get the search posts page */
+router.get('/searchPosts', function(req, res, next) {
+  res.render('searchPostsView', {});
+});
+
+
+/*Get the make a post page */
+router.get('/makeAPost', function(req, res, next) {
+  res.render('makeAPostView', {});
+});
 
 
 /* post prediction result page (called by submitting the form)*/
@@ -51,7 +61,7 @@ router.post('/predict', function (req, res, next) {
     //console.log(obj);
     if (obj.status === "ZERO_RESULTS") {
       //the API call did not return any results
-      res.render('errorView', {place: req.body.location});
+      res.render('predictErrorView', {place: req.body.location});
     } else {
       //the API call successfully returned a result
       var formattedPlace = obj.candidates[0].formatted_address;
@@ -120,11 +130,19 @@ function predictQuality(token, lat, long, place, res){
     var rating = prediction.features[0].properties.quality_percent;
     var quality = prediction.features[0].properties.quality;
     var temperature = prediction.features[0].properties.temperature;
+    var time = "";
+    if(prediction.features[0].properties.dusk !== null){
+      time = prediction.features[0].properties.dusk.civil;
+    }
+    else{
+      time = prediction.features[0].properties.dawn.civil;
+    }
+    time = time.substring(time.indexOf("T") + 1, time.length - 1);
 
     console.log(prediction.features[0].properties);
 
     //RENDER THE RESULT PAGE
-    res.render('predictionView', {place: place, latitude: lat, longitude: long, type: predictionType, rating: rating, quality: quality, temp: temperature});
+    res.render('predictionView', {place: place, latitude: lat, longitude: long, type: predictionType, rating: rating, quality: quality, temp: temperature, time: time});
 
 
   });
@@ -132,6 +150,112 @@ function predictQuality(token, lat, long, place, res){
 }
 
 
+
+/* post viewPosts result page (called by submitting the form)*/
+router.post('/viewPosts', function (req, res, next) {
+
+  //REQUEST TO GOOGLE API TO TURN PLACE INTO COORDINATES
+  var request = require("request");
+  var options = {
+    method: 'GET',
+    url: 'https://maps.googleapis.com/maps/api/place/findplacefromtext/json',
+    qs:
+        {
+          input: req.body.location, //whatever was typed into the input form
+          inputtype: 'textquery',
+          fields: 'formatted_address,name,rating,opening_hours,geometry',
+          key: config.gmapsKey },
+
+    headers:
+        {
+          'Postman-Token': '4a6773b8-b2b2-4278-b59b-65a3528bc9db',
+          'cache-control': 'no-cache'
+        }
+  };
+
+  request(options, function (error, response, body) {
+    if (error) { //there was an error with the API call
+      throw new Error(error);
+    }
+    //there was no error with the API call
+    var obj = JSON.parse(body); //parsed the json response
+    //console.log(body);
+    //console.log(obj);
+    if (obj.status === "ZERO_RESULTS") {
+      //the API call did not return any results
+      res.render('searchPostsErrorView', {place: req.body.location});
+    } else {
+      //the API call successfully returned a result
+      var formattedPlace = obj.candidates[0].formatted_address;
+      var locationObj = obj.candidates[0].geometry.location;
+
+      //SEARCH THE DATABASE FOR THE CLOSEST POSTS TO THE LOCATION (maybe only show the closest 10?)
+      res.render('viewPostsView', {place: formattedPlace});
+    }
+
+  });
+
+
+});
+
+
+
+
+
+/* Make a post (called from submitting the form)*/
+router.post('/SubmitPost', function (req, res, next) {
+
+  //REQUEST TO GOOGLE API TO TURN PLACE INTO COORDINATES
+  var request = require("request");
+  var options = {
+    method: 'GET',
+    url: 'https://maps.googleapis.com/maps/api/place/findplacefromtext/json',
+    qs:
+        {
+          input: req.body.location, //whatever was typed into the input form
+          inputtype: 'textquery',
+          fields: 'formatted_address,name,rating,opening_hours,geometry',
+          key: config.gmapsKey },
+
+    headers:
+        {
+          'Postman-Token': '4a6773b8-b2b2-4278-b59b-65a3528bc9db',
+          'cache-control': 'no-cache'
+        }
+  };
+
+  request(options, function (error, response, body) {
+    if (error) { //there was an error with the API call
+      throw new Error(error);
+    }
+    //there was no error with the API call
+    var obj = JSON.parse(body); //parsed the json response
+    //console.log(body);
+    //console.log(obj);
+    if (obj.status === "ZERO_RESULTS") {
+      //the API call did not return any results
+      res.render('makePostErrorView', {place: req.body.location});
+    } else {
+      //the API call successfully returned a result
+      var formattedPlace = obj.candidates[0].formatted_address;
+      var locationObj = obj.candidates[0].geometry.location;
+
+      //Store the post in the datbase so that others will see it when they search
+      var lat = locationObj.lat;
+      var long = locationObj.lng;
+      var username = req.body.username; //SHOULD BE OBTAINED FROM THE USER'S GOOGLE OAUTH
+      var date = req.body.date;
+      var pic = req.body.pic; //how to store the images in correct format?
+      console.log(pic);
+
+      //render the success view telling the user their post has been made
+      res.render('successPostView', {place: formattedPlace});
+    }
+
+  });
+
+
+});
 
 
 module.exports = router;
