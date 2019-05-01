d2f0ccf92bc0dc8ab9fa39170af015529e877638
radelmo <radelmo@bu.edu>
2019-05-01 17:55:37 -0400

    Oauth calls back

--- project/SunsetPredictor/routes/google-auth.js
+++ project/SunsetPredictor/routes/google-auth.js
@@ -1,80 +1,50 @@
-/** import { google } from 'googleapis';
-
-const googleConfig = {
-    clientId: '1078333857284-9icej2t3a4bria2l1cfqpa74baqpgonj.apps.googleusercontent.com',
-    clientSecret: 'xFJete1hukDi_eb4uu_4dNkX',
-    redirect: 'localhost:3000/' // this must match your google api settings
-};
-
-
-const defaultScope = [
-    'https://www.googleapis.com/auth/plus.me',
-    'https://www.googleapis.com/auth/userinfo.email',
-];
+var express = require('express');
+var router = express.Router();
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth2').Strategy;
+const users = {};
+const config = require("./config");
+const app = express();
+const User = require('../models/user-model');
 
 
-//Get a url which will open the google sign-in page and request access to the scope provided (such as calendar events).
 
-function createConnection() {
-    return new google.auth.OAuth2(
-        googleConfig.clientId,
-        googleConfig.clientSecret,
-        googleConfig.redirect
-    );
-}
+passport.serializeUser(function(user, done) {
+    done(null, user);
+});
 
-function getConnectionUrl(auth) {
-    return auth.generateAuthUrl({
-        access_type: 'offline',
-        prompt: 'consent',
-        scope: defaultScope
+passport.deserializeUser((id, done)=>{
+    User.findById(id).then((user) => {
+        done(null, user);
     });
-}
 
-function getGooglePlusApi(auth) {
-    return google.plus({ version: 'v1', auth });
-}
+});
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(
+    new GoogleStrategy({
+            clientID: config.googleConfig.clientId,
+            clientSecret: config.googleConfig.clientSecret,
+            callbackURL: "/auth/google/callback",
+            //passReqToCallback   : true
+        },
+        (accessToken, refreshToken, profile,  done)=>{
+            //passport callback function
+            console.log(profile);
+            User.findOne()
+            res.redirect("/");
+        })
+)
 
 
-//MAIN
 
 
 
-//Create a Google URL and send to the client to log in the user.
-
-function urlGoogle() {
-    const auth = createConnection();
-    const url = getConnectionUrl(auth);
-    return url;
-}
-
-
-//Take the "code" parameter which Google gives us once when the user logs in, then get the user's email and id.
-
-async function getGoogleAccountFromCode(code) {
-    const data = await auth.getToken(code);
-    const tokens = data.tokens;
-    const auth = createConnection();
-    auth.setCredentials(tokens);
-    const plus = getGooglePlusApi(auth);
-    const me = await plus.people.get({userId: 'me'});
-    const userGoogleId = me.data.id;
-    const userGoogleEmail = me.data.emails && me.data.emails.length && me.data.emails[0].value;
-    return {
-        id: userGoogleId,
-        email: userGoogleEmail,
-        tokens: tokens,
-    };
-}
 
 module.exports = {
-    googleConfig: googleConfig,
-    defaultScope: defaultScope,
-    createConnection: createConnection(),
-    getConnectionUrl: getConnectionUrl(),
-    getGooglePlusApi: getGooglePlusApi(),
-    getGoogleAccountFromCode: getGoogleAccountFromCode
-}
-
-/**
+    router,
+    passport,
+    users
+};
 
--- project/SunsetPredictor/routes/index.js
+++ project/SunsetPredictor/routes/index.js
@@ -7,131 +7,174 @@ var http = require("http");
 var util = require('util');
 var exec = require('child_process').exec;
 
+const mongoose = require("mongoose");
+
 var config = require('./config');
-//const googleAuth = require('./google-auth');
 
+const googleAuth = require('./google-auth');
+const app = express();
+const passport = require('passport');
+const cookieSession = require("cookie-session");
 
-/* GET home page. (called by default) */
-router.get('/', function(req, res, next) {
+const bodyParser=require('body-parser');
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
 
-  res.render('index', {});
+app.use(cookieSession({
+  maxAge: 24 * 60 * 60 *1000,
+  keys:[config.mongodb.cookieKey]
+}));
+
+mongoose.connect(config.mongodb.dbURI, {useNewUrlParser: true }, () => {
+  console.log('connected to mongodb');
 });
 
 
+//googleAuth.setup(app);
+app.use(passport.initialize());
+app.use(passport.session());
 
-/* post prediction result page (called by submitting the form)*/
-router.post('/predict', function (req, res, next) {
+router.get('/google-auth',
+    passport.authenticate('google', {
+      scope : ['https://www.googleapis.com/auth/plus.login']
+    }));
 
-  //REQUEST TO GOOGLE API TO TURN PLACE INTO COORDINATES
-  var request = require("request");
-  var options = {
-    method: 'GET',
-    url: 'https://maps.googleapis.com/maps/api/place/findplacefromtext/json',
-    qs:
-        {
-          input: req.body.location, //whatever was typed into the input form
-          inputtype: 'textquery',
-          fields: 'formatted_address,name,rating,opening_hours,geometry',
-          key: config.gmapsKey },
 
-    headers:
-        {
-          'Postman-Token': '4a6773b8-b2b2-4278-b59b-65a3528bc9db',
-          'cache-control': 'no-cache'
-        }
-  };
+router.get('/auth/google/callback', function (req, res) {
+  console.log("GOOGLE CALLBACK");
+  passport.authenticate('google', function (err, profile) {
+    res.redirect("/");
+  })(req, res); // you to call the function retuned by passport.authenticate, with is a midleware.
+});
 
-  request(options, function (error, response, body) {
-    if (error) { //there was an error with the API call
-      throw new Error(error);
-    }
-    //there was no error with the API call
-    var obj = JSON.parse(body); //parsed the json response
-    //console.log(body);
-    //console.log(obj);
-    if (obj.status === "ZERO_RESULTS") {
-      //the API call did not return any results
-      res.render('errorView', {place: req.body.location});
-    } else {
-      //the API call successfully returned a result
-      var formattedPlace = obj.candidates[0].formatted_address;
-      var locationObj = obj.candidates[0].geometry.location;
 
-      //LOGIN TO SUNBURST API AND RETRIEVE TEMPORARY ACCESS TOKEN IN ORDER TO MAKE A PREDICTION (predict called within login)
-      sunburstLogin(config.sbUsername, config.sbPassword, locationObj.lat, locationObj.lng, formattedPlace, res);
+  /* GET home page. (called by default) */
+router.get('/', function (req, res, next) {
 
-    }
-
+    res.render('index', {});
   });
 
 
-});
+  /* post prediction result page (called by submitting the form)*/
+  router.post('/predict', function (req, res, next) {
+
+    //REQUEST TO GOOGLE API TO TURN PLACE INTO COORDINATES
+    var request = require("request");
+    var options = {
+      method: 'GET',
+      url: 'https://maps.googleapis.com/maps/api/place/findplacefromtext/json',
+      qs:
+          {
+            input: req.body.location, //whatever was typed into the input form
+            inputtype: 'textquery',
+            fields: 'formatted_address,name,rating,opening_hours,geometry',
+            key: config.gmapsKey
+          },
+
+      headers:
+          {
+            'Postman-Token': '4a6773b8-b2b2-4278-b59b-65a3528bc9db',
+            'cache-control': 'no-cache'
+          }
+    };
+
+    request(options, function (error, response, body) {
+      if (error) { //there was an error with the API call
+        throw new Error(error);
+      }
+      //there was no error with the API call
+      var obj = JSON.parse(body); //parsed the json response
+      //console.log(body);
+      //console.log(obj);
+      if (obj.status === "ZERO_RESULTS") {
+        //the API call did not return any results
+        res.render('errorView', {place: req.body.location});
+      } else {
+        //the API call successfully returned a result
+        var formattedPlace = obj.candidates[0].formatted_address;
+        var locationObj = obj.candidates[0].geometry.location;
+
+        //LOGIN TO SUNBURST API AND RETRIEVE TEMPORARY ACCESS TOKEN IN ORDER TO MAKE A PREDICTION (predict called within login)
+        sunburstLogin(config.sbUsername, config.sbPassword, locationObj.lat, locationObj.lng, formattedPlace, res);
+
+      }
+
+    });
+
+
+  });
 
 //generates temporary access token and obtains a prediction object from prediction function
-function sunburstLogin(username, password, lat, long, place, res){
-  var token;
-  var loginCommand = 'curl -X "POST" "https://sunburst.sunsetwx.com/v1/login" \\\n' +
-      '  -u "' + username + ':' + password + '" \\\n' +
-      '  -d "grant_type=password"\n'
+  function sunburstLogin(username, password, lat, long, place, res) {
+    var token;
+    var loginCommand = 'curl -X "POST" "https://sunburst.sunsetwx.com/v1/login" \\\n' +
+        '  -u "' + username + ':' + password + '" \\\n' +
+        '  -d "grant_type=password"\n';
 
-  childLogin = exec(loginCommand, function(error, stdout, stderr) {
+    childLogin = exec(loginCommand, function (error, stdout, stderr) {
 
-    console.log('LOGIN stdout: ' + stdout);
-    console.log('LOGIN stderr: ' + stderr);
+      console.log('LOGIN stdout: ' + stdout);
+      console.log('LOGIN stderr: ' + stderr);
 
-    if (error !== null) {
-      throw new Error(error);
-      console.log('exec error: ' + error);
-    }
-    console.log(JSON.parse(stdout));
-    var loginObj = JSON.parse(stdout);
-    token = loginObj.access_token;
-    predictQuality(token, lat, long, place, res);
-  });
+      if (error !== null) {
+        throw new Error(error);
+        console.log('exec error: ' + error);
+      }
+      console.log(JSON.parse(stdout));
+      var loginObj = JSON.parse(stdout);
+      token = loginObj.access_token;
+      predictQuality(token, lat, long, place, res);
+    });
 
 
-
-}
+  }
 
 //returns a parsed json object of the prediction
-function predictQuality(token, lat, long, place, res){
-  var coordString = lat + "," + long; //abides by sunburst's coordinate format
+  function predictQuality(token, lat, long, place, res) {
+    var coordString = lat + "," + long; //abides by sunburst's coordinate format
 
-  var qualityCommand = 'curl "https://sunburst.sunsetwx.com/v1/quality" \
-  -d "geo='+ coordString + '" \
+    var qualityCommand = 'curl "https://sunburst.sunsetwx.com/v1/quality" \
+  -d "geo=' + coordString + '" \
   -H "Authorization: Bearer ' + token + '" \
   -G';
 
-  childQuality = exec(qualityCommand, function(error, stdout, stderr){
+    childQuality = exec(qualityCommand, function (error, stdout, stderr) {
 
-    console.log('QUALITY stdout: ' + stdout);
-    console.log('QUALITY stderr: ' + stderr);
+      console.log('QUALITY stdout: ' + stdout);
+      console.log('QUALITY stderr: ' + stderr);
 
-    if(error !== null)
-    {
-      throw new Error(error);
-      console.log('exec error: ' + error);
-    }
-    console.log("QUALITY OBJECT:");
-    console.log(JSON.parse(stdout));
-    var prediction = JSON.parse(stdout);
+      if (error !== null) {
+        throw new Error(error);
+        console.log('exec error: ' + error);
+      }
+      console.log("QUALITY OBJECT:");
+      console.log(JSON.parse(stdout));
+      var prediction = JSON.parse(stdout);
 
-    var predictionType = prediction.features[0].properties.type;
-    var rating = prediction.features[0].properties.quality_percent;
-    var quality = prediction.features[0].properties.quality;
-    var temperature = prediction.features[0].properties.temperature;
+      var predictionType = prediction.features[0].properties.type;
+      var rating = prediction.features[0].properties.quality_percent;
+      var quality = prediction.features[0].properties.quality;
+      var temperature = prediction.features[0].properties.temperature;
 
-    console.log(prediction.features[0].properties);
+      console.log(prediction.features[0].properties);
 
-    //RENDER THE RESULT PAGE
-    res.render('predictionView', {place: place, latitude: lat, longitude: long, type: predictionType, rating: rating, quality: quality, temp: temperature});
+      //RENDER THE RESULT PAGE
+      res.render('predictionView', {
+        place: place,
+        latitude: lat,
+        longitude: long,
+        type: predictionType,
+        rating: rating,
+        quality: quality,
+        temp: temperature
+      });
 
 
-  });
+    });
 
-}
-
-
-
+  }
 
 module.exports = router;
+
+
+
